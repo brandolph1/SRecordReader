a2ade1b465eeade0682f4cf84909661557fcd24d
Brad Randolph <brad.randolph@welchallyn.com>
2013-10-22 16:02:54 -0400

    Initial check in

---
+++ SRecordReader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WelchAllyn.SRecord;
+using WelchAllyn.Nand512Library;
+
+namespace WelchAllyn.SRecordReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
